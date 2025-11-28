de11a5709c9bbf9271374002d3cf3bd9f21396ec
mitsutoshi <mitsu3204@gmail.com>
2025-11-28 22:22:45 +0900

    update

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -8,23 +8,35 @@ return {
     config = function()
 
       require("mason").setup()
+      local mason_lspconfig = require("mason-lspconfig")
+      local servers = { "gopls", "pyright", "rust_analyzer" }
+      --mason_lspconfig.setup({
+      --  ensure_installed = servers,
+      --  handlers = {},
+      --})
 
-      local function on_attach(_, bufnr)
+      local lspconfig = require("lspconfig")
+
+      local function on_attach(client, bufnr)
+        if client.name == "rust_analyzer" then
+          client.server_capabilities.documentFormattingProvider = false
+        end
+
         local map = vim.keymap.set
         local opts = { buffer = bufnr, noremap = true, silent = true }
+
         map("n", "gd", vim.lsp.buf.definition, opts)
-        map("n", "gD", vim.lsp.buf.declaration, opts)
-        map("n", "gi", vim.lsp.buf.implementation, opts)
-        map("n", "gr", vim.lsp.buf.references, opts)
-        map("n", "K",  vim.lsp.buf.hover, opts)
+        map("n", "<leader>h", vim.lsp.buf.hover, opts)
+        map("n", "<leader>i", vim.lsp.buf.implementation, opts)
+        map("n", "<leader>r", vim.lsp.buf.references, opts)
         map("n", "<leader>rn", vim.lsp.buf.rename, opts)
         map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
         map("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, opts)
         map("n", "<leader>d", vim.diagnostic.open_float, { noremap = true, silent = true })
       end
 
-      -- Go
-      vim.lsp.config('gopls', {
+
+      lspconfig.gopls.setup {
           on_attach = on_attach,
           settings = {
             gopls = {
@@ -33,24 +45,23 @@ return {
               completeUnimported = true,
             },
           },
-      })
+      }
 
-      -- Python
-      vim.lsp.config('pyright', {
-          on_attach = on_attach,
-      })
-
-      -- Rust
-      vim.lsp.config('rust_analyzer', {
+      lspconfig.rust_analyzer.setup {
           on_attach = on_attach,
           settings = {
               ["rust-analyzer"] = {
                   cargo = { allFeatures = true },
                   check = { command = "clippy" },
+                  formatting = { enable = true },
               },
           },
-      })
+      }
+
+      lspconfig.pyright.setup {
+          on_attach = on_attach,
+      }
+
     end,
   }
 }
-
